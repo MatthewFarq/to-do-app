9f0512f79f7aaa7e6c2a1ea1d946d6409c0a61c0
Matthew Farquharson <matthewfarquharson@gmail.com>
2020-02-24 21:16:20 -0600

    Add delete functionality and mdl styles

--- app.js
+++ app.js
@@ -20,11 +20,34 @@ function onReady() {
     //set the input's type to checkbox
     checkbox.type = 'checkbox';
 
-    //set the title
-    newLi.textContent = title;
+    //create delete button with icon
+    let button = document.createElement('button');
+    button.classList.add('delete-button', 'mdl-button', 'mdl-js-button', 'mdl-button--icon');
 
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
+    let buttonIcon = document.createElement('i');
+    buttonIcon.classList.add('material-icons');
+    buttonIcon.textContent = 'clear';
+
+    //append icon to button
+    button.appendChild(buttonIcon);
+
+    //add event listener to delete button
+    button.addEventListener('click', event => {
+      event.preventDefault();
+      toDoList.removeChild(newLi);
+    });
+
+    //combine input text and checkbox in one div
+    let titleSpan = document.createElement('span');
+    titleSpan.textContent = title;
+
+    let titleCheckContainer = document.createElement('div');
+    titleCheckContainer.appendChild(checkbox);
+    titleCheckContainer.appendChild(titleSpan);
+
+    //attach the elements to the li
+    newLi.appendChild(titleCheckContainer);
+    newLi.appendChild(button);
 
     //attach the li to the ul
     toDoList.appendChild(newLi);
@@ -35,6 +58,6 @@ function onReady() {
 }
 
 window.onload = function () {
-  alert('The window has loaded!');
+  // alert('The window has loaded!');
   onReady();
 };
